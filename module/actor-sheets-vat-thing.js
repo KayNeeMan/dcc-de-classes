e6171d5e1733747b59ed66024c1787ba4cccd1ab
KayNeeMan <124114485+KayNeeMan@users.noreply.github.com>
2023-02-02 08:59:20 -0500

    Update actor-sheets-vat-thing.js

--- module/actor-sheets-vat-thing.js
+++ module/actor-sheets-vat-thing.js
@@ -1,26 +1,25 @@
-/*
-DCC DE Vat-Thing character sheet overrides
+/**
+* DCC DE Vat-Thing character sheet overrides
 */
 
 import DCCActorSheet from '/system/dcc/module/actor-sheet.js'
 
-/*
-Extend the zero-level/NPC sheet for Vat-Thing
-@extends {DCCActorSheet}
-*/
-
+/**
+ * Extend the zero-level/NPC sheet for Vat-Thing
+ * @extends {DCCActorSheet}
+ */
 class ActorSheetVatThing extends DCCActorSheet{
-	//@overrides
-	async getData (options) {
-		const data = await super.getData(options)
-		this.options.template = 'modules/dcc-de-classes/templates/actor-sheet-vat-thing.html'
-		if (data.system.details.sheetClass !== 'Vat-Thing') {
-			this.actor.update({
-				'data.class.className': game.il8n.localize('VatThing.VatThing')
-		})
+	/**@overrides */
+		async getData (options) {
+	const data = await super.getData(options)
+	this.options.template = 'modules/dcc-de-classes/templates/actor-sheet-vat-thing.html'
+	if (data.system.details.sheetClass !== 'Vat-Thing') {
+	  this.actor.update({
+	    'data.class.className': game.il8n.localize('VatThing.VatThing')
+	  })
 	}
 	return data
-	}
+      }
 }
 
 export {
